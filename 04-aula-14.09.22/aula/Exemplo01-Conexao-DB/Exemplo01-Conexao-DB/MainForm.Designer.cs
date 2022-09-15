d0404aec9cad20be53b7239d78b851e8d4e1844a
Cleyson Silva <cleysonsilva.me@gmail.com>
2022-09-14 21:47:11 -0300

    Exemplo 1 - Sala - Aula 04

---
+++ 04-aula-14.09.22/aula/Exemplo01-Conexao-DB/Exemplo01-Conexao-DB/MainForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Exemplo01_Conexao_DB
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxCourses = new System.Windows.Forms.ListBox();
+            this.lbxStudents = new System.Windows.Forms.ListBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbxCourses
+            // 
+            this.lbxCourses.FormattingEnabled = true;
+            this.lbxCourses.Location = new System.Drawing.Point(32, 132);
+            this.lbxCourses.Name = "lbxCourses";
+            this.lbxCourses.Size = new System.Drawing.Size(262, 186);
+            this.lbxCourses.TabIndex = 0;
+            // 
+            // lbxStudents
+            // 
+            this.lbxStudents.FormattingEnabled = true;
+            this.lbxStudents.Location = new System.Drawing.Point(481, 132);
+            this.lbxStudents.Name = "lbxStudents";
+            this.lbxStudents.Size = new System.Drawing.Size(262, 186);
+            this.lbxStudents.TabIndex = 1;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(350, 214);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Consultar";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lbxStudents);
+            this.Controls.Add(this.lbxCourses);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxCourses;
+        private System.Windows.Forms.ListBox lbxStudents;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ 04-aula-14.09.22/aula/Exemplo01-Conexao-DB/Exemplo01-Conexao-DB/MainForm.cs
@@ -0,0 +1,87 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exemplo01_Conexao_DB
+{
+    public partial class MainForm : Form
+    {
+        private MySqlConnection _conn;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            string connStr = "server=localhost;user=root;database=escola;port=3306;password=root";
+            _conn = new MySqlConnection(connStr);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _conn.Open();
+
+            string sql = "SELECT * FROM CURSOS ORDER BY abrev";
+
+            MySqlCommand command = new MySqlCommand(sql, _conn);
+
+            
+            MySqlDataReader reader = command.ExecuteReader();
+
+            lbxCourses.Items.Clear();
+
+            while (reader.Read())
+            {
+                lbxCourses.Items.Add(reader.GetString("abrev"));
+            }
+
+
+
+            _conn.Close();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String value = (String)lbxCourses.SelectedItem;
+
+                String sql = "SELECT * FROM alunos WHERE curso=@curso";
+                MySqlCommand command = new MySqlCommand(sql, _conn);
+                command.Parameters.AddWithValue("curso", value);
+
+                _conn.Open();
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                lbxStudents.Items.Clear();
+                while (reader.Read())
+                { 
+                    lbxStudents.Items.Add(reader.GetString("nome"));
+                }
+
+                if (lbxStudents.Items.Count == 0)
+                {
+                    lbxStudents.Items.Add("Sem alunos encontrados!");
+
+                }
+
+                _conn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                lbxStudents.Items.Clear();
+                lbxStudents.Items.Add("Erro ao buscar os alunos. " + ex.Message);
+
+            }
+        }
+    }
+}

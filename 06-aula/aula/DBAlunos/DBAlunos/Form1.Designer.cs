abaf9ecba4f203eaa7006832ae723fa7b9db721b
Cleyson Silva <cleysonsilva.me@gmail.com>
2022-09-28 21:34:08 -0300

    aula 06 - banco de dados universidade

---
+++ 06-aula/aula/DBAlunos/DBAlunos/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace DBAlunos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblCurso = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCurso = new System.Windows.Forms.TextBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(142, 44);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(185, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Cadastro de Alunos";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(67, 114);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(16, 13);
+            this.lblId.TabIndex = 1;
+            this.lblId.Text = "Id";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(67, 153);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblCurso
+            // 
+            this.lblCurso.AutoSize = true;
+            this.lblCurso.Location = new System.Drawing.Point(67, 190);
+            this.lblCurso.Name = "lblCurso";
+            this.lblCurso.Size = new System.Drawing.Size(34, 13);
+            this.lblCurso.TabIndex = 3;
+            this.lblCurso.Text = "Curso";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(107, 111);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(157, 20);
+            this.txtId.TabIndex = 4;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(107, 150);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(157, 20);
+            this.txtNome.TabIndex = 5;
+            // 
+            // txtCurso
+            // 
+            this.txtCurso.Location = new System.Drawing.Point(107, 187);
+            this.txtCurso.Name = "txtCurso";
+            this.txtCurso.Size = new System.Drawing.Size(157, 20);
+            this.txtCurso.TabIndex = 6;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(306, 104);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 7;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(306, 133);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 8;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(306, 162);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 9;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(306, 191);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 10;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 261);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.txtCurso);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblCurso);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblCurso;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCurso;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnExcluir;
+    }
+}
+
---
+++ 06-aula/aula/DBAlunos/DBAlunos/Form1.cs
@@ -0,0 +1,174 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Windows.Forms;
+
+namespace DBAlunos
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection _conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            MySqlConnectionStringBuilder connBuilder = new MySqlConnectionStringBuilder();
+
+            connBuilder.Server = "localhost";
+            connBuilder.UserID = "root";
+            connBuilder.Password = "root";
+            connBuilder.Database = "universidade";
+            connBuilder.Port = 3306;
+
+            bool includeCredentials = true;
+
+            _conn = new MySqlConnection(connBuilder.GetConnectionString(includeCredentials));
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _conn.Open();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String sql = "INSERT INTO alunos VALUES (@id, @nome, @curso)";
+                MySqlCommand command = new MySqlCommand(sql, _conn);
+                command.Parameters.AddWithValue("id", txtId.Text);
+                command.Parameters.AddWithValue("nome", txtNome.Text);
+                command.Parameters.AddWithValue("curso", txtCurso.Text);
+
+
+                if (command.ExecuteNonQuery() == 0)
+                {
+                    MessageBox.Show("Erro ao cadastrar um aluno", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                else
+                {
+                    MessageBox.Show("Aluno cadastrado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    txtId.Clear();
+                    txtNome.Clear();
+                    txtCurso.Clear();
+                }
+
+
+
+
+                command.Dispose();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String sql = "SELECT * FROM alunos WHERE id=@id";
+                MySqlCommand command = new MySqlCommand(sql, _conn);
+                command.Parameters.AddWithValue("id", txtId.Text);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                if (!reader.Read())
+                {
+                    MessageBox.Show("Aluno não encontrado", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+
+                    txtNome.Text = reader.GetString("nome");
+                    txtCurso.Text = reader.GetString("curso");
+                }
+
+
+
+
+                command.Dispose();
+                reader.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String sql = "UPDATE alunos SET nome=@nome, curso=@curso WHERE id=@id";
+                MySqlCommand command = new MySqlCommand(sql, _conn);
+                command.Parameters.AddWithValue("id", txtId.Text);
+                command.Parameters.AddWithValue("nome", txtNome.Text);
+                command.Parameters.AddWithValue("curso", txtCurso.Text);
+
+
+                if (command.ExecuteNonQuery() == 0)
+                {
+                    MessageBox.Show("Aluno não encontrado", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    MessageBox.Show("Aluno atualizado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    txtId.Clear();
+                    txtNome.Clear();
+                    txtCurso.Clear();
+                }
+
+
+
+                command.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String sql = "DELETE FROM alunos WHERE id=@id";
+                MySqlCommand command = new MySqlCommand(sql, _conn);
+                command.Parameters.AddWithValue("id", txtId.Text);
+                command.Parameters.AddWithValue("nome", txtNome.Text);
+                command.Parameters.AddWithValue("curso", txtCurso.Text);
+
+
+                if (command.ExecuteNonQuery() == 0)
+                {
+                    MessageBox.Show("Aluno não encontrado", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    MessageBox.Show("Aluno deletado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    txtId.Clear();
+                    txtNome.Clear();
+                    txtCurso.Clear();
+                }
+
+
+
+                command.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+    }
+}
